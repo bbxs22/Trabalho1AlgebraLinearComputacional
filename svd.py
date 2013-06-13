ce7e93ae8273a3ef2a5f48cd6230a78b255c7f51
Bruna <Xavier>
2013-06-12 22:49:19 -0300

    SVD utiliza arquivo exportado pelo matlab

--- svd.py
+++ svd.py
@@ -1,29 +1,82 @@
-import math
-from qr import *
 from matrix import *
 
 class SVD:
 
-    @staticmethod
-    def calculate_eigenvalues_eigenvectors(matrix, error=0.000001):
-        '''
-        Calcula autovalores de uma matriz quadrada simetrica
-        @param float (optional)
-        @param Matrix
-        @return tuple<int,list,Matrix>
-        '''
-        iterations = 0
-        previous_value, current_value = 1, 0
-        eigenvectors = matrix.eye(matrix.rows(), matrix.rows())
-        while abs(current_value - previous_value) > error:
-            Q, R = QR.execute(matrix)
-            matrix = R * Q
-            eigenvectors = eigenvectors * Q
-            iterations = iterations + 1
-            previous_value, current_value = current_value, matrix.get(1, 0)
+    MATLAB_U = 'matlab/u.txt'
+    MATLAB_S = 'matlab/s.txt'
+    MATLAB_V = 'matlab/v.txt'
 
-        eigenvalues = list()
-        for j in xrange(matrix.columns()):
-            eigenvalues.append(matrix.get(j, j))
+    @staticmethod
+    def read_matrix(file_name):
+        '''
+        Le a matriz de um arquivo
+        @param str
+        @return Matrix
+        '''
+        temp, rows, values = list(), 0, list()
+        file = open(file_name)
+        for line in file:
+            temp = map(float, line.split())
+            values += temp
+            rows = rows + 1
+        return Matrix(values, rows, len(temp))
+
+    def __init__(self):
+        self.u = SVD.read_matrix(SVD.MATLAB_U)
+        self.s = SVD.read_matrix(SVD.MATLAB_S)
+        self.v = SVD.read_matrix(SVD.MATLAB_V)
+        
+        values = list()
+        for i in xrange(self.s.rows()):
+            values.append(self.s.get(i, i))
+        self.score_vector = Vector(values, False)
+        
+    def get_query_vector(self, terms_position):
+        '''
+        Recupera o vetor correspondente a consulta
+        O vetor consulta eh dado pela soma coordenada a coordenada de cada vetor coluna que representa o termo
+        @param list<int>
+        @return Vector
+        '''
+        size = self.u.columns()
+        query_vector = Vector([0.0]*size, False)
+        
+        for position in terms_position:
+            if position > 0:
+                query_vector = query_vector + self.u.get_row(position)
             
-        return iterations, eigenvalues, eigenvectors
+        return query_vector
+        
+    def calculate_score(self, query_vector, matrix):
+        '''
+        Recupera o vetor de score
+        @param Vector
+        @param Matrix
+        @return Vector
+        '''
+        # recupera os vetores-linha da matriz
+        vectors = self.get_row_vectors(matrix)
+            
+        # ajusta o score de 0 para cada um dos termos
+        scores = Vector([0.0] * len(vectors))
+            
+        # calcula o score para cada elemento de vector
+        s = 0
+        for vector in vectors:
+            for i in xrange(query_vector.columns()):
+                score = query_vector.get(i) * vector.get(i) * self.score_vector.get(i)
+                scores.set(s, scores.get(s) + score)
+            s = s + 1 
+        
+        return scores
+        
+    def get_row_vectors(self, matrix):
+        '''
+        Recupera a lista de vetores linha da matriz
+        @param Matrix
+        @return list<Vector>
+        '''
+        vectors = list()
+        for i in xrange(matrix.rows()):
+            vectors.append(matrix.get_row(i))
+        return vectors
--- testSVD.py
+++ testSVD.py
@@ -1,76 +1,50 @@
 from svd import *
-from qr import *
-from householder import *
 import unittest
 
 class TestSVD(unittest.TestCase):
 
-    def test_calculate_eigenvalues_eigenvectors(self):
-        A = Matrix([52, 30, 49, 28, 30, 50, 8, 44, 49, 8, 46, 16, 28, 44, 16, 22], 4, 4)
-        #AT = A.transpose()
-        #AAT = A * AT
-        
-        # transforma matriz simetrica em uma tridiagonal
-        AAT = Householder.execute(AAT)
-        
-        # encontra os autovalores via QR
-        iterations, eigenvalues, eigenvectors = SVD.calculate_eigenvalues_eigenvectors(AAT)
-        
-        self.assertAlmostEqual(eigenvalues[0], 132.6279, delta=0.000001)
-        self.assertAlmostEqual(eigenvalues[1], 52.4423, delta=0.000001)
-        self.assertAlmostEqual(eigenvalues[2], -11.54113, delta=0.000001)
-        self.assertAlmostEqual(eigenvalues[3], -3.52904, delta=0.000001)
-        
-#        self.assertAlmostEqual(eigenvectors.get(0, 0), 1.0, delta=0.000001)
-#        self.assertAlmostEqual(eigenvectors.get(0, 1), 1.0, delta=0.000001)
-#        self.assertAlmostEqual(eigenvectors.get(1, 0), 1.0, delta=0.000001)
-#        self.assertAlmostEqual(eigenvectors.get(1, 1), -1.0, delta=0.000001)
-    
-    def test_calculate_eigenvalues_eigenvectors_rectangular_matrix(self):
-        A = Matrix([3, 1, 1, -1, 3, 1], 2, 3)
-        AT = A.transpose()
-        AAT = A * AT
-        
-        self.assertAlmostEqual(AAT.get(0, 0), 11.0, delta=0.000001)
-        self.assertAlmostEqual(AAT.get(0, 1), 1.0, delta=0.000001)
-        self.assertAlmostEqual(AAT.get(1, 0), 1.0, delta=0.000001)
-        self.assertAlmostEqual(AAT.get(1, 1), 11.0, delta=0.000001)
-        
-        # transforma matriz simetrica em uma tridiagonal
-        AAT = Householder.execute(AAT)
-        
-        # encontra os autovalores via QR
-        iterations, eigenvalues, eigenvectors = SVD.calculate_eigenvalues_eigenvectors(AAT)
+    def test_new_svd(self):
+        SVD.MATLAB_U = 'test/svd/u.txt'
+        SVD.MATLAB_S = 'test/svd/s.txt'
+        SVD.MATLAB_V = 'test/svd/v.txt'
 
-        self.assertAlmostEqual(AAT.get(0, 0), 11.0, delta=0.000001)
-        self.assertAlmostEqual(AAT.get(0, 1), 1.0, delta=0.000001)
-        self.assertAlmostEqual(AAT.get(1, 0), 1.0, delta=0.000001)
-        self.assertAlmostEqual(AAT.get(1, 1), 11.0, delta=0.000001)
-        
-        self.assertAlmostEqual(eigenvalues[0], 12.0, delta=0.000001)
-        self.assertAlmostEqual(eigenvalues[1], 10.0, delta=0.000001)
-        
-        self.assertAlmostEqual(eigenvectors.get(0, 0), 1.0, delta=0.000001)
-        self.assertAlmostEqual(eigenvectors.get(0, 1), 1.0, delta=0.000001)
-        self.assertAlmostEqual(eigenvectors.get(1, 0), 1.0, delta=0.000001)
-        self.assertAlmostEqual(eigenvectors.get(1, 1), -1.0, delta=0.000001)
+        svd = SVD()
+        self.assertAlmostEqual(svd.score_vector.get(0), 3.0024077, delta=0.000001)
+        self.assertAlmostEqual(svd.score_vector.get(1), 2.9208098, delta=0.000001)
 
-    def test_calculate_eigenvalues_eigenvectors_square_matrix(self):
-        A = Matrix([2, 0, 8, 6, 0, 1, 6, 0, 1, 7, 5, 0, 7, 4, 0, 7, 0, 8, 5, 0, 0, 10, 0, 0, 7], 5, 5)
-        AT = A.transpose()
-        AAT = A * AT
+    def test_read_matrix(self):
+        matrix = SVD.read_matrix('test/svd/test_read_matrix.txt')
+        self.assertEqual(matrix.rows(), 2)
+        self.assertEqual(matrix.columns(), 2)
+        self.assertAlmostEqual(matrix.get(0, 0), 3.002407, delta=0.000001)
+        self.assertAlmostEqual(matrix.get(0, 1), 0.0, delta=0.000001)
+        self.assertAlmostEqual(matrix.get(1, 0), 0.0, delta=0.000001)
+        self.assertAlmostEqual(matrix.get(1, 1), 2.920809, delta=0.000001)
         
-        # transforma matriz simetrica em uma tridiagonal
-        AAT = Householder.execute(AAT)
+    def test_get_query_vector(self):
+        SVD.MATLAB_U = 'test/svd/u.txt'
+        SVD.MATLAB_S = 'test/svd/s.txt'
+        SVD.MATLAB_V = 'test/svd/v.txt'
+
+        svd = SVD()
+        vector = svd.get_query_vector([3, 4])
+        self.assertAlmostEqual(vector.get(0), -0.3847023, delta=0.000001)
+        self.assertAlmostEqual(vector.get(1), -0.2466640, delta=0.000001)
         
-        # encontra os autovalores via QR
-        iterations, eigenvalues, eigenvectors = SVD.calculate_eigenvalues_eigenvectors(AAT)
-            
-        self.assertAlmostEqual(eigenvalues[0], 321.07, delta=0.1)
-        self.assertAlmostEqual(eigenvalues[1], 230.17, delta=0.1)
-        self.assertAlmostEqual(eigenvalues[2], 12.70, delta=0.1)
-        self.assertAlmostEqual(eigenvalues[3], 3.94, delta=0.1)
-        self.assertAlmostEqual(eigenvalues[4], 0.12, delta=0.1)
+    def get_row_vectors(self):
+        SVD.MATLAB_U = 'test/svd/u.txt'
+        SVD.MATLAB_S = 'test/svd/s.txt'
+        SVD.MATLAB_V = 'test/svd/v.txt'
+
+        svd = SVD()
+        doc_vectors = svd.get_row_vectors(svd.v)
+        self.assertEqual(len(doc_vectors), 9)
+        self.assertAlmostEqual(doc_vectors[0].get(0), -0.4595774, delta=0.000001)
+        self.assertAlmostEqual(doc_vectors[0].get(1), -0.3474577, delta=0.000001)
+        self.assertAlmostEqual(doc_vectors[1].get(0), -0.5578699, delta=0.000001)
+        self.assertAlmostEqual(doc_vectors[1].get(1), -0.2612275, delta=0.000001)
+        self.assertAlmostEqual(doc_vectors[2].get(0), -0.2303408, delta=0.000001)
+        self.assertAlmostEqual(doc_vectors[2].get(1), -0.1590020, delta=0.000001)
 
 if __name__ == '__main__':
     unittest.main()
